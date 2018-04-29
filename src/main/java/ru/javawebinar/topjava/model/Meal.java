6cb520c8cf2c9a7d3e4c04974cf9fb01556efb25
lagsterius <lagsterius@gmail.com>
2018-04-29 22:00:56 +0300

    HW1

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,16 +5,28 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal {
+    private Integer id;
     private final LocalDateTime dateTime;
-
     private final String description;
-
     private final int calories;
+
+    public Meal(int id, LocalDateTime dateTime, String description, int calories) {
+        this(dateTime, description, calories);
+        this.id = id;
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
@@ -35,5 +47,14 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return ((Meal) obj).getId() == id;
     }
 }
